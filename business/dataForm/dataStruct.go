b4d3305564ab6d8a9ef0d69d8f0b19140e706748
tongWz <tongweizhen@foxmail.com>
2021-10-20 09:38:29 +0800

    xiugai

--- business/dataForm/dataStruct.go
+++ business/dataForm/dataStruct.go
@@ -1,4 +1,4 @@
-package queue
+package dataForm
 
 type BaseForm struct {
 	Api string `json:"api"`
--- business/scrm/ScrmUserReport.go
+++ business/scrm/ScrmUserReport.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"github.com/streadway/amqp"
 	"golang-dts/business"
+	"golang-dts/business/dataForm"
 	"golang-dts/pkg/logging"
-	"golang-dts/pkg/queue"
 )
 
 // interface 的方法实现必须要通过new(struct) 我们直接继承通用结构体 来实现InterfaceQueue
@@ -19,10 +19,11 @@ func (m *UserReportHandle) SetLogName() string {
 }
 
 func (m *UserReportHandle) Delivery(msg amqp.Delivery) {
-	message := new(queue.ScrmUserReportFrom)
-	// isAck := true
+	message := new(dataForm.ScrmUserReportFrom)
+	//isAck := true
 	if err := json.Unmarshal(msg.Body, message); err != nil {
 		logging.Fatal("scrm用户上报数据解析失败：", string([]byte(msg.Body)))
+		logging.Fatal("scrm用户上报数据解析失败：", err)
 		return
 	}
 	fmt.Println(message)
